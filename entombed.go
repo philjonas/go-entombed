2727929aa5945d1670c2f220e79f871b2e0607fa
philjonas <philjonascode@gmail.com>
2020-12-26 16:04:15 +0000

    the magic bit is on! :)

--- entombed.go
+++ entombed.go
@@ -3,9 +3,9 @@ package entombed
 import "math/rand"
 
 const (
-	IS_WALL     = 1
-	NO_WALL     = 0
-	RANDOM_WALL = -1
+	isWall     = 1
+	noWall     = 0
+	randomWall = -1
 )
 
 func getRandomBit() uint {
@@ -18,13 +18,118 @@ func isZeroPresent(lastRows []uint) bool {
 			return true
 		}
 	}
-
 	return false
 }
 
+func doTheMagicBitNow(lasttwo, threeabove uint) int {
+	var response int
+	lasttwo = lasttwo << 3
+	switch lasttwo | threeabove {
+	case 0b00000:
+		response = isWall
+		break
+	case 0b00001:
+		response = isWall
+		break
+	case 0b00010:
+		response = isWall
+		break
+	case 0b00011:
+		response = randomWall
+		break
+	case 0b00100:
+		response = noWall
+		break
+	case 0b00101:
+		response = noWall
+		break
+	case 0b00110:
+		response = randomWall
+		break
+	case 0b00111:
+		response = randomWall
+		break
+	case 0b01000:
+		response = isWall
+		break
+	case 0b01001:
+		response = isWall
+		break
+	case 0b01010:
+		response = isWall
+		break
+	case 0b01011:
+		response = isWall
+		break
+	case 0b01100:
+		response = randomWall
+		break
+	case 0b01101:
+		response = noWall
+		break
+	case 0b01110:
+		response = noWall
+		break
+	case 0b01111:
+		response = noWall
+		break
+	case 0b10000:
+		response = isWall
+		break
+	case 0b10001:
+		response = isWall
+		break
+	case 0b10010:
+		response = isWall
+		break
+	case 0b10011:
+		response = randomWall
+		break
+	case 0b10100:
+		response = noWall
+		break
+	case 0b10101:
+		response = noWall
+		break
+	case 0b10110:
+		response = noWall
+		break
+	case 0b10111:
+		response = noWall
+		break
+	case 0b11000:
+		response = randomWall
+		break
+	case 0b11001:
+		response = noWall
+		break
+	case 0b11010:
+		response = isWall
+		break
+	case 0b11011:
+		response = randomWall
+		break
+	case 0b11100:
+		response = randomWall
+		break
+	case 0b11101:
+		response = noWall
+		break
+	case 0b11110:
+		response = noWall
+		break
+	case 0b11111:
+		response = noWall
+		break
+	default:
+		break
+	}
+
+	return response
+}
+
 /*
-	int MAGIC(unsigned int lasttwo, unsigned int threeabove);
 public:
-	std::string render_line(unsigned int row);
-	void rowGen(std::vector<unsigned int> &lastrows);
+	std::string render_line(unsigned int row)
+	void rowGen(std::vector<unsigned int> &lastrows)
 */
--- entombed_test.go
+++ entombed_test.go
@@ -20,12 +20,58 @@ func TestIsZeroPresent(t *testing.T) {
 			lastRows: []uint{0, 1, 2},
 			expected: true,
 		},
+		{
+			lastRows: []uint{1, 2},
+			expected: false,
+		},
+		{
+			lastRows: []uint{0},
+			expected: true,
+		},
+		{
+			lastRows: []uint{},
+			expected: false,
+		},
 	}
 
 	for _, test := range tests {
 		result := isZeroPresent(test.lastRows)
 		if result != test.expected {
 			t.Errorf("isZeroPresent(%v), expected: %t, got: %t", test.lastRows, test.expected, result)
+		}
+	}
+}
+
+type doTheMagicBitNowTestCase struct {
+	lasttwo    uint
+	threeabove uint
+	expected   int
+}
+
+func TestDoTheMagicBitNow(t *testing.T) {
+	tests := []doTheMagicBitNowTestCase{
+		{
+			lasttwo:    0b11,
+			threeabove: 0b111,
+			expected:   noWall,
+		},
+		{
+			lasttwo:    0b00,
+			threeabove: 0b000,
+			expected:   isWall,
+		},
+		{
+			lasttwo:    0b00,
+			threeabove: 0b110,
+			expected:   randomWall,
+		},
+	}
+
+	for _, test := range tests {
+		result := doTheMagicBitNow(test.lasttwo, test.threeabove)
+		if result != test.expected {
+			t.Errorf("doTheMagicBitNow(%b, %b), expected: %d, got: %d",
+				test.lasttwo, test.threeabove, test.expected, result)
 		}
 	}
 }
